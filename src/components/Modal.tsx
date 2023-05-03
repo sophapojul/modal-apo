4167d3be9c150bb8afe4ec75f481e5713fa489bf
Pascal Allau <pascal.allau@zohomail.eu>
2023-05-03 22:16:37 +0200

    feature:remove title and footer props

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PropsWithChildren, ReactNode, useRef } from "react";
+import { PropsWithChildren, useRef } from "react";
 
 import useCloseOnOverlay from "../hooks/useCloseOnOverlay";
 import useCloseOnEscapeKey from "../hooks/useCloseOnEscapeKey";
@@ -39,10 +39,6 @@ const Content = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 1);
 `;
 
-const Section = styled.div`
-  margin: 0;
-`;
-
 const Body = styled.div`
   padding: 10px;
   border-top: 1px solid #eee;
@@ -67,32 +63,22 @@ const Button = styled.button`
   font-size: 1.2em;
 `;
 
-const Title = styled.h2`
-  margin: 0;
-`;
-
 interface ModalProps {
   isOpen: boolean;
-  title?: ReactNode | string;
   onClose: () => void;
-  footer?: ReactNode | string;
 }
 
 /**
  * React component for a modal dialog
  * @param isOpen - boolean
- * @param title - ReactNode | string
  * @param onClose - function
  * @param children - ReactNode
- * @param footer - ReactNode | string
  * @returns React component
  */
 export function Modal({
   isOpen,
-  title,
   onClose,
   children,
-  footer,
 }: PropsWithChildren<ModalProps>) {
   const overlayRef = useRef(null);
   useCloseOnOverlay(onClose, overlayRef);
@@ -109,16 +95,10 @@ export function Modal({
         aria-labelledby="successDialog"
         aria-describedby="dialog_desc"
       >
-        <Section>
-          <Title id="successDialog" className="modal__title">
-            {title}
-          </Title>
-          <Button type="button" className="modal__button" onClick={onClose}>
-            X
-          </Button>
-        </Section>
+        <Button type="button" className="modal__button" onClick={onClose}>
+          X
+        </Button>
         <Body id="dialog_desc">{children}</Body>
-        <Section>{footer}</Section>
       </Content>
     </StyledModal>
   );
